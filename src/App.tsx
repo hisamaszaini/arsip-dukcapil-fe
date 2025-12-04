d27ca0bf39aaf0db6125ddbf63df3f61f77bafc6
hisamaszaini <muhammadhisamaszaini@gmail.com>
2025-12-04 10:09:54 +0700

    Clean structure to dynamic arsip

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,7 @@ import LoginPage from './pages/Login';
 import AdminDashboardPage from './pages/Dashboard';
 import ProfilePage from './pages/Profile';
 import UserPage from './pages/User';
-import AktaKelahiranPage from './pages/AktaKelahiran';
-import SuratKehilanganPage from './pages/SuratKehilangan';
-import AktaKematianPage from './pages/AktaKematian';
 import OperatorDashboardPage from './pages/operator/DashboardOperator';
-import SuratPermohonanPindahPage from './pages/SuratPermohonanPindah';
-import SuratPerubahanKependudukanPage from './pages/SuratPerubahanKependudukan';
 import HomePage from './pages/Home';
 import KategoriPage from './pages/Kategori';
 import ArsipDynamicPage from './pages/ArsipDynamic';
@@ -42,22 +37,11 @@ function App() {
               <Route path="/admin/dashboard" element={<AdminDashboardPage />} />
               <Route path="/admin/user" element={<UserPage />} />
               <Route path="/admin/kategori" element={<KategoriPage />} />
-              <Route path="/admin/layanan-arsip/akta-kelahiran" element={<AktaKelahiranPage />} />
-              <Route path="/admin/layanan-arsip/akta-kematian" element={<AktaKematianPage />} />
-              <Route path="/admin/layanan-arsip/surat-kehilangan" element={<SuratKehilanganPage />} />
-              <Route path="/admin/layanan-arsip/surat-permohonan-pindah" element={<SuratPermohonanPindahPage />} />
-              <Route path="/admin/layanan-arsip/surat-perubahan-kependudukan" element={<SuratPerubahanKependudukanPage />} />
-              {/* Dynamic Arsip Route */}
               <Route path="/admin/arsip/:slug" element={<ArsipDynamicPage />} />
             </Route>
 
             <Route element={<ProtectedRoute allowedRoles={['OPERATOR']}> <Outlet /> </ProtectedRoute>} >
               <Route path="/operator/dashboard" element={<OperatorDashboardPage />} />
-              <Route path="/operator/layanan-arsip/akta-kelahiran" element={<AktaKelahiranPage />} />
-              <Route path="/operator/layanan-arsip/akta-kematian" element={<AktaKematianPage />} />
-              <Route path="/operator/layanan-arsip/surat-kehilangan" element={<SuratKehilanganPage />} />
-              <Route path="/operator/layanan-arsip/surat-permohonan-pindah" element={<SuratPermohonanPindahPage />} />
-              <Route path="/operator/layanan-arsip/surat-perubahan-kependudukan" element={<SuratPerubahanKependudukanPage />} />
               <Route path="/operator/arsip/:slug" element={<ArsipDynamicPage />} />
             </Route>
           </Route>
